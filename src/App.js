3492f5d05faaf38c3376ddda10dbe26e1b0fb8aa
amirhilal044 <amiramirhilal@gmail.com>
2024-09-13 08:31:31 +0300

    added images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import AddTutorial from './components/AddTutorial';
+import NavBar from './components/NavBar';
 import ProtectedRoute from './components/ProtectedRoute';
 import TutorialDetail from './components/TutorialDetail';
 import DashboardPage from './pages/DashboardPage';
@@ -10,6 +11,7 @@ import TutorialsPage from './pages/TutorialsPage';
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         {/* Public routes */}
         <Route path="/login" element={<LoginPage />} />
