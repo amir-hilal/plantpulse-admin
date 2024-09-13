1b482a4f29cf91e9ca8659481a0951996f4a05b4
amirhilal044 <amiramirhilal@gmail.com>
2024-09-13 08:18:13 +0300

    protected pages in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import AddTutorial from './components/AddTutorial';
+import ProtectedRoute from './components/ProtectedRoute';
 import TutorialDetail from './components/TutorialDetail';
 import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
 import TutorialsPage from './pages/TutorialsPage';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          {/* Define routes for each page */}
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/tutorials" element={<TutorialsPage />} />
-          <Route path="/add-tutorial" element={<AddTutorial />} />
-          <Route path="/tutorial/:id" element={<TutorialDetail />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/tutorials"
+          element={
+            <ProtectedRoute>
+              <TutorialsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-tutorial"
+          element={
+            <ProtectedRoute>
+              <AddTutorial />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/tutorial/:id"
+          element={
+            <ProtectedRoute>
+              <TutorialDetail />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
