31be67390dae67d10c739d80d8122fd0bdeff8fd
amirhilal044 <amiramirhilal@gmail.com>
2024-09-13 08:15:58 +0300

    created authSlice

---
+++ src/redux/authSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../services/api';
+
+// Async action for logging in
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await api.post('/login', { email, password });
+      const { token } = response.data;
+      localStorage.setItem('token', token);
+      return token;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    token: localStorage.getItem('token') || null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    logout: (state) => {
+      localStorage.removeItem('token');  
+      state.token = null;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.token = action.payload;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  }
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tutorialsReducer from './tutorialsSlice';
+import authReducer from './authSlice';
 
 const store = configureStore({
   reducer: {
-    tutorials: tutorialsReducer
-  }
+    tutorials: tutorialsReducer,
+    auth: authReducer,
+  },
 });
 
 export default store;
